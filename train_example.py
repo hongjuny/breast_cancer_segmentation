11f20f7578bd5bad74c118c958c8f6b63a5d29be
Hong-Jun Yoon <hongjuny72@hotmail.com>
2020-06-11 15:18:14 -0400

    unet model

---
+++ train_example.py
@@ -0,0 +1,25 @@
+from model import *
+from data import *
+
+
+### train
+data_gen_args = dict(rotation_range=0.2,
+                    width_shift_range=0.05,
+                    height_shift_range=0.05,
+                    shear_range=0.05,
+                    zoom_range=0.05,
+                    horizontal_flip=True,
+                    fill_mode='nearest')
+
+myGene = trainGenerator( 2,'/Users/ay5/Documents/git/unet/data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+model = unet()
+model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model.fit_generator(myGene,steps_per_epoch=2000,epochs=5,callbacks=[model_checkpoint])
+
+
+### test
+testGene = testGenerator("/Users/ay5/Documents/git/unet/data/membrane/test")
+model = unet()
+model.load_weights("unet_membrane.hdf5")
+results = model.predict_generator(testGene,30,verbose=1)
+saveResult("/Users/ay5/Documents/git/unet/data/membrane/test",results)
