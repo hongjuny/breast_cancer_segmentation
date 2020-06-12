0148ed189a656e8bdcf7181149e98609b22282b5
Yoon, Hong-Jun <yoonh@ornl.gov>
2020-06-12 07:45:30 -0400

    added test data

--- train_example.py
+++ train_example.py
@@ -11,15 +11,15 @@ data_gen_args = dict(rotation_range=0.2,
                     horizontal_flip=True,
                     fill_mode='nearest')
 
-myGene = trainGenerator( 2,'/Users/ay5/Documents/git/unet/data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+myGene = trainGenerator( 2,'/home/ay5/Documents/git/unet/data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=2000,epochs=5,callbacks=[model_checkpoint])
 
 
 ### test
-testGene = testGenerator("/Users/ay5/Documents/git/unet/data/membrane/test")
+testGene = testGenerator("/home/ay5/Documents/git/unet/data/membrane/test")
 model = unet()
 model.load_weights("unet_membrane.hdf5")
 results = model.predict_generator(testGene,30,verbose=1)
-saveResult("/Users/ay5/Documents/git/unet/data/membrane/test",results)
+saveResult("/home/ay5/Documents/git/unet/data/membrane/test",results)
